740761960e8d6b779251823e11d994644a7f844c
ThejKishore <kishores1984@gmail.com>
2020-01-11 14:00:23 -0600

    added dekorator for building the k8s yaml

--- kubegateway/build.gradle.kts
+++ kubegateway/build.gradle.kts
@@ -1,9 +1,13 @@
+
 plugins {
 	id ("org.springframework.boot") version "2.2.2.RELEASE"
 	id ("io.spring.dependency-management") version "1.0.8.RELEASE"
 	id("com.bmuschko.docker-spring-boot-application") version "6.1.1"
 	java
 }
+
+val ap4kVersion="0.4.1";
+val dekorateVersion="0.10.4";
 
 group = "com.kish.learning"
 version = "0.0.1-SNAPSHOT"
@@ -24,6 +28,16 @@ dependencies {
 
 	implementation("org.springframework.cloud","spring-cloud-starter-kubernetes-all")
 
+//	compile("io.ap4k:kubernetes-spring-starter:${ap4kVersion}")
+//	annotationProcessor("io.ap4k:kubernetes-annotations:${ap4kVersion}")
+
+	implementation("io.dekorate:dekorate-spring-boot:${dekorateVersion}")
+	implementation("io.dekorate:kubernetes-annotations:${dekorateVersion}")
+	annotationProcessor("io.dekorate:kubernetes-annotations:${dekorateVersion}")
+	annotationProcessor("io.dekorate:dekorate-spring-boot:${dekorateVersion}")
+	annotationProcessor("org.springframework.boot:spring-boot-starter-actuator") //We need `dekorate` to see the actuator classes
+
+//	annotationProcessor("io.ap4k:ap4k-spring-boot:${ap4kVersion}")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude ("org.junit.vintage","junit-vintage-engine")
 	}
--- kubegateway/src/main/java/com/kish/learning/kubegateway/KubegatewayApplication.java
+++ kubegateway/src/main/java/com/kish/learning/kubegateway/KubegatewayApplication.java
@@ -1,14 +1,28 @@
 package com.kish.learning.kubegateway;
 
+//import io.dekorate.kubernetes.annotation.*;
+//import io.dekorate.prometheus.annotation.EnableServiceMonitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
 
+
+//@KubernetesApplication (
+		/*ports = {
+				@Port(name="kubegatewayservice",containerPort = 9002,hostPort = 39002,protocol = Protocol.TCP)
+		},
+		serviceType = ServiceType.NodePort,
+		envVars = {
+				@Env(name = "key1", configmap = "my-config", value = "key1"),
+				@Env(name = "key2", secret="my-secret", value = "key2")
+		}*/
+
+//)
+//@EnableServiceMonitor(port = "http", path="/prometheus", interval=20)
 @SpringBootApplication
 @EnableDiscoveryClient
 public class KubegatewayApplication {
